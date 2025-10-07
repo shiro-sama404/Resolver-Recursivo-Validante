240ee390415d106bfa4c4b242852bf0ae4add7fb
Arthur Barbosa <arthurbarbosaandrade2003@gmail.com>
2025-10-07 00:42:48 -0400

    Atualização da main
    Atualização da Main para suporte à cache daemon

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
+#include "server/cache_daemon.h"
 #include "cli/arguments.h"
 #include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
     try {
         Arguments args(argc, argv);
-        args.print_summary();
 
-        if (args.get_mode() == Mode::Validating) {
-            cout << "DNSSEC ativado\n";
+        switch (args.get_cache_command()) {
+            case CacheCommand::Activate:
+                cout << "Iniciando Cache Daemon..." << endl;
+                {
+                    CacheDaemon daemon;
+                    daemon.run();
+                }
+                return 0;
+
+            case CacheCommand::Status:
+                CacheDaemon::send_command("STATUS");
+                return 0;
+
+            case CacheCommand::PurgeAll:
+                CacheDaemon::send_command("PURGE_ALL");
+                return 0;
+
+            case CacheCommand::Deactivate:
+                CacheDaemon::send_command("DEACTIVATE");
+                return 0;
+
+            default:
+                args.print_summary();
+                // Fluxo de resolução do DNS
+                return 0;
         }
     }
-    catch (const std::exception& ex) {
-        cerr << "Erro: " << ex.what() << "\n";
+    catch (const exception& e) {
+        cerr << "Erro: " << e.what() << endl;
         return 1;
     }
-    return 0;
 }
