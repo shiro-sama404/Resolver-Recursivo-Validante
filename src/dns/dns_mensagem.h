18068023ef328ee85c6fe2197be55985071f3ca5
Fernanda Neves <fernanda_neves@ufms.br>
2025-10-07 21:14:01 -0400

    Implementação Inicial da classe dns_mensagem.h
    Usando a RFC 1035 ela declara todos os atributos do cabecalho e pergunta do dns, e já declara quais são as funções que o dns precisa implementar,
    
    inicializar
    montar query ( que é a pesquisa que ele vai mandar pro servidor)
    receber a resposta(que é o endereço de IP)
    e imprimir a resposta pro usuário

---
+++ src/dns/dns_mensagem.h
@@ -0,0 +1,39 @@
+#ifndef DNSMENSAGEM_H
+#define DNSMENSAGEM_H
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+struct CabecalhoDNS {
+    uint16_t id;      
+    uint16_t flags;    
+    uint16_t qdcount;  
+    uint16_t ancount;  
+    uint16_t nscount;  
+    uint16_t arcount;  
+};
+
+// o site em si que ele quer pesquisar
+struct PerguntaDNS { 
+    std::string qname;
+    uint16_t qtype;    
+    uint16_t qclass;   
+};
+
+class DNSMensagem {
+public:
+    CabecalhoDNS cabecalho;
+    PerguntaDNS pergunta;
+
+    DNSMensagem();
+
+    std::vector<uint8_t> montarQuery();
+
+    void parseResposta(const std::vector<uint8_t>& dados);
+
+    void imprimirResposta();
+
+};
+
+#endif 
